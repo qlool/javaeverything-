b481cb00b67624b44dab5fefb6d879bcb942ce4e
cxn <1353337123@qq.com>
2018-03-16 09:17:05 +0800

    one

---
+++ src/com/qlool/test/ThreadSleepTest.java
@@ -0,0 +1,15 @@
+package com.qlool.test;
+
+public class ThreadSleepTest {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        try {
+            System.out.println("startTime = " +startTime);
+            Thread.sleep(75000);
+            long endTime = System.currentTimeMillis();
+            System.out.println("endTime ="  +endTime);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
