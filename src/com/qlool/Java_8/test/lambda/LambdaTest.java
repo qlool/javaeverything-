9aecae34176fe102396bd067bf386b863559c037
cxn <1353337123@qq.com>
2018-03-16 09:45:02 +0800

    lambda

---
+++ src/com/qlool/Java_8/test/lambda/LambdaTest.java
@@ -0,0 +1,35 @@
+package com.qlool.Java_8.test.lambda;
+
+/**
+ * @Description : lambda java_8 Characteristics
+ * @Time in 2018年3月16日 09:32:56
+ * @Author By cxn
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+        olderRun();
+        newRun();
+    }
+
+    /**
+     * 匿名内部类:
+     */
+    public static void  olderRun(){
+        // Java 8之前：
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Before Java8, too much code for too little to do");
+            }
+        }).start();
+    }
+
+    /**
+     * lambda 方式：
+     */
+    public static void newRun(){
+        new Thread(() -> System.out.println("In Java8, Lambda expression rocks !!")).start();
+    }
+    
+}
--- src/com/qlool/thread/test/ThreadSleepTest.java
+++ src/com/qlool/thread/test/ThreadSleepTest.java
@@ -4,11 +4,11 @@ public class ThreadSleepTest {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         try {
-            System.out.println("startTime = " +startTime);
+            System.out.println("startTime = " + startTime);
             Thread.sleep(75000);
             long endTime = System.currentTimeMillis();
-            System.out.println("endTime ="  +endTime);
-        }catch (Exception e){
+            System.out.println("endTime =" + endTime);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
