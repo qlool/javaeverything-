812b52011f97ea894e3a9b856d21485abe9a3352
chenxinan <1353337123@qq.com>
2018-04-27 14:05:48 +0800

    git tag 测试 2.0.0版本

---
+++ src/com/qlool/com/qlool/git/test/GitTagTest.java
@@ -0,0 +1,12 @@
+package com.qlool.com.qlool.git.test;
+
+/**
+ *
+ * 测试git tag 用法
+ */
+public class GitTagTest {
+
+    public static void main(String[] args) {
+        System.out.println("git tag version-2.0.0");
+    }
+}
